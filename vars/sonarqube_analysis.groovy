658f38cbc20a5e74cc69a3607db618a17b639316
Prasad <pj344504@gmail.com>
2026-08-01 01:04:37 +0530

    Fix sonarqube_analysis function

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,3 +1,11 @@
-def call() {
-    echo "sonarqube_analysis() called"
+def call(String sonarName, String projectKey, String projectName) {
+
+    withSonarQubeEnv(sonarName) {
+        sh """
+            ${tool 'Sonar'}/bin/sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.sources=.
+        """
+    }
 }
